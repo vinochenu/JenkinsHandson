129f8103b9f7b82fd07cc8d8a69106008aaf126e
e3027405 <e3027405@BLR1-LTBPKSX52.FNFIS.com>
2020-07-16 12:25:02 +0530

    Initial Commit

---
+++ JenkinsHandson/src/com/jenkinsHandson/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.jenkinsHandson;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
